df28bb627be68e76c6cbebc163c7bec36da1a2e2
seungbin-park <tmdqls0212@naver.com>
2020-04-21 12:11:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-
 from bs4 import BeautifulSoup
 import requests
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbsparta
-
- 
-
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://series.naver.com/novel/top100List.nhn',headers=headers)
+data = requests.get('https://series.naver.com/novel/detail.nhn?productNo=1200383',headers=headers)
 
 
 soup = BeautifulSoup(data.text, 'html.parser')
-novels = soup.select('ul.comic_top_lst > li')
-for novel in novels:
-    image = novel.select_one('a > img')
-    print(image['src'])
+soup.find(' ul.comment_focus > li')
+
+for review in reviews:
+    content = review.select_one('p')
+    print(review)
+#1. nick name 가져오기
+#2. 내용 가져오기(cbox)
+#3. 가져온 자료 보이게 하기 
+
+
+
+
+
 
 
