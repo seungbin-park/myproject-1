7e869cbb5bee10b3f02e4670b3fcfc4e9a2016b8
hyunsung_kim <hyunsung.kim@superb-ai.com>
2020-04-21 22:06:20 +0900

    Get comment from given urlA

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://series.naver.com/novel/detail.nhn?productNo=1200383',headers=headers)
+import os
+import time
 
+options = webdriver.ChromeOptions() 
+options.add_argument('headless') 
+options.add_argument('window-size=1920x1080') 
+options.add_argument("disable-gpu") 
 
-soup = BeautifulSoup(data.text, 'html.parser')
-soup.find(' ul.comment_focus > li')
+path = os.path.dirname(__file__)
+driver = webdriver.Chrome(path + '/chromedriver', options=options)
 
-for review in reviews:
-    content = review.select_one('p')
-    print(review)
-#1. nick name 가져오기
-#2. 내용 가져오기(cbox)
-#3. 가져온 자료 보이게 하기 
+# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get('https://series.naver.com/novel/detail.nhn?productNo=1200383',headers=headers)
 
+driver.get('https://series.naver.com/novel/detail.nhn?productNo=1200383')
+time.sleep(2)
 
+# 해당 페이지의 html 소스 선택
+html = driver.page_source
+# html 파싱하기
+soup = BeautifulSoup(html, 'html.parser')
+comments = soup.select_one('#comment_focus')
 
+for review in comments.select('div.cbox_comment_area'):
+    #1. nick name 가져오기
+    nick_name = review.select_one("span.cbox_nick_name").text
 
+    #2. 내용 가져오기(cbox)
+    content = review.select_one("div.cbox_desc_comment>p").text
 
+    #3. 가져온 자료 보이게 하기(??)
+    print(nick_name, content)
 
-
+driver.quit()
