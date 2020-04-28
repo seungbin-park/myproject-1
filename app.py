fb0da067f0e91e4f5a16f067b8806a5e66586179
seungbin-park <tmdqls0212@naver.com>
2020-04-28 14:35:00 +0900

    qwert
    qwert

--- app.py
+++ app.py
@@ -5,21 +5,38 @@ app = Flask(__name__)
 
 from pymongo import MongoClient  
 
-client = MongoClient('localhost', 27017) 
+client = MongoClient('52.78.73.206',username="test", password='test',authSource='admin', authMechanism='SCRAM-SHA-1')
 db = client.dbsparta 
-
 
 
 @app.route('/')
 def home():
-    return render_template('')
+    return render_template('index.html')
+
+
+@app.route('/rofan')
+def rofan():
+    return render_template('index2.html')
+
+@app.route('/fantagy')
+def fantagy():
+    return render_template('index3.html')
+
+@app.route('/marsharl')
+def marsharl():
+    return render_template('index4.html')
+
+
+@app.route('/review/<int:review_num>')
+def reviews(review_num):
+    return render_template(f'review{review_num}.html')
+
 
 
 @app.route('/reviews', methods=['POST'])
 def write_review():
     author_receive = request.form['author_give']
     review_receive = request.form['review_give']
-    
 
     review = {
         'author': author_receive,
@@ -28,7 +45,7 @@ def write_review():
     }
 
     db.reviews.insert_one(review)
-    return jsonify({'result': 'success', 'msg': '주문이 성공적으로 작성되었습니다.'})
+    return jsonify({'result': 'success', 'msg': '리뷰가 성공적으로 작성되었습니다.'})
 
 
 @app.route('/reviews', methods=['GET'])
